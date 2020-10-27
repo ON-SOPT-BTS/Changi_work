9db6dff8c10bf6133f05665c22cdeda5a8b8fb14
Changi <limchangi@naver.com>
2020-10-27 21:09:52 +0900

    level3 수정

--- homework_2/level3.js
+++ homework_2/level3.js
@@ -5,18 +5,23 @@ const crypto = require('crypto');
 // if(!fs.existsSync(`/${dir}`)){
 //     fs.mkdirSync(`/${dir}`);
 // }
+
 const numArr = [1, 2, 3, 4, 5];
 const fileCommonName = 'PassWord';
 
 numArr.forEach((num) => {
-    crypto.randomBytes(64, (err, buf) => {
-        const salt = buf.toString('base64');
-        crypto.pbkdf2('비밀번호', salt, 100000, 64, 'sha512', (err, key) => {
-            const password = key.toString('base64');
-            const fileName = fileCommonName + num;
-            fs.writeFile(`${fileName}.txt`, password, () => {
-                console.log(`${fileName} 작성 완료`);
-            })
+    const fileName = fileCommonName + num;
+    const password = new Promise((resolve,reject)=>{
+        crypto.randomBytes(64, (err, buf) => {
+            const salt = buf.toString('base64');
+            crypto.pbkdf2('비밀번호', salt, 100000, 64, 'sha512', (err, key) => {
+                resolve(key.toString('base64'));
+            });
         });
-    });
+    })    
+    password.then((pw)=>{
+        fs.writeFile(`${fileName}.txt`,pw,()=>{
+            console.log(`${fileName} 작성 완료`)
+        })
+    })
 })
